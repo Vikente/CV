3f7deb1d506a43e1c56bd1bdbd0dd32d144c4d18
Vikentiii <96529640+Vikente@users.noreply.github.com>
2022-02-15 12:52:31 +0300

    new: project

---
+++ ОсновыJS.js
@@ -0,0 +1,13 @@
+function Vika(num){
+    if (num === 0) {
+        return console.log('У Вики понос.')
+     }
+    else if (num % 2 === 0){
+    return console.log('Я тебя люблю, Викуся!')
+    }
+    
+    else {
+        return console.log('Ты самая лучшая и красивая!')
+    }
+}
+Vika(0)
